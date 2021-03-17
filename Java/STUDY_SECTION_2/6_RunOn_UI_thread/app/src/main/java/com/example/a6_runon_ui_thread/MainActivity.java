1406e0c242fc85f479ebdb01964470780690666a
hwan <36683832+SungHwan-Jang@users.noreply.github.com>
2021-03-18 01:03:37 +0900

    6_run_on_ui_thread

--- Java/STUDY_SECTION_2/6_RunOn_UI_thread/app/src/main/java/com/example/a6_runon_ui_thread/MainActivity.java
+++ Java/STUDY_SECTION_2/6_RunOn_UI_thread/app/src/main/java/com/example/a6_runon_ui_thread/MainActivity.java
@@ -23,6 +23,11 @@ public class MainActivity extends AppCompatActivity {
 
         textView1 = findViewById(R.id.text_view1);
         textView2 = findViewById(R.id.text_view2);
+
+        //thread 가동
+        isRunning = true;
+        ThreadClass thread = new ThreadClass();
+        thread.start();
     }
 
     public void btn_method(View view){
@@ -30,14 +35,30 @@ public class MainActivity extends AppCompatActivity {
         textView1.setText(MessageFormat.format("now : {0}", now));
     }
 
-    class ThreadClass extends Thread{
+    class ThreadClass extends Thread {
         @Override
         public void run() {
             super.run();
-            SystemClock.sleep(100);
+            while (isRunning) {
+                SystemClock.sleep(100);
+                // 한 번 선언한 뒤 변하지 않는 Immutable 형식
+                // Java 에서는 상수를 선언할 때 final을 이용하여 Read-Only 로 설정
+                final long time = System.currentTimeMillis();
+                Log.d("test", "time : " + time);
 
-            long time = System.currentTimeMillis();
-            Log.d("test", "time : " + time);
+                // 기존에는 handler를 사용하여 화면처리를 구현하였다.
+                // !!!!!! handler 를 사용하는경우 : 이곳 저곳에서 핸들러에 등록된 처리를 사용할 경우
+                // !!!!!! runOnUithread를 사용 경우 : 한 군데에서만 사용하는 경우
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        // 실제로는 time 변수는 개발자 생성 스레드에서 만들어 진 변수이며
+                        // runOnUiThread는 main thread에서 처리한다.
+                        // 안정성을 위하여 해당 time 변수는 final로 선언해야 한다.
+                        textView2.setText(MessageFormat.format("thread : {0}", time));
+                    }
+                }); // runnable 인터페이스를 구현한 객체를 전달,  thread와 관련됨 즉, 익명 중첩 클래스를 만들어 전달하면 된다.
+            }
         }
     }
 
